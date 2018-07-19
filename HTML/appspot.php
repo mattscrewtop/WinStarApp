fcc96dc7f18ca5d7ebbd4eb7a4acf9d1d191c77e
mattscrewtop <mferrell@screwtopmedia.com>
2018-07-19 13:57:12 -0500

    appspot.php

---
+++ HTML/appspot.php
@@ -0,0 +1,21 @@
+<?php
+
+$curl = curl_init("https://www.screwtopmedia.com/home");
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_HEADER, 0);              // Do not set any custom headers.
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);      // Listen for a response.
+//curl_setopt($curl, CURLOPT_POST, 1);                // Send data over POST.
+//curl_setopt($curl, CURLOPT_POSTFIELDS, $post);      // Attach the list of fields to the request.
+//curl_setopt($curl, CURLOPT_HTTPHEADER, Array("Content-Type: application/x-www-form-urlencoded"));
+$resp = curl_exec($curl);
+curl_close ($curl);	
+if (count($resp) > 0) {
+	//echo $resp;
+	echo 'screwtop loaded<br/>';
+}
+
+
+?>
+<script>
+open(location, '_self').close();
+</script>
